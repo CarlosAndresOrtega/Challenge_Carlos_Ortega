5bff2a10b16cee1172124e06443330f46d366b84
CarlosAndresOrtega <caortegayate@gmail.com>
2023-05-08 22:20:56 -0500

    Configurado el state management redux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const taskState= useSelector(state=>state.tasks)
+  console.log(taskState)
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+    {
+        id: "1",
+        title: "Task 1",
+        description: "Task 1 description",
+        completed: false,
+    },
+    {
+        id: "2",
+        title: "Task 2",
+        description: "Task 2 description",
+        completed: false,
+    }
+]
+
+export const taskSlice = createSlice({
+    name: 'tasks',
+    initialState,
+    reducers: {
+      increment: (state) => {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the Immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+        state.value += 1
+      },
+      decrement: (state) => {
+        state.value -= 1
+      },
+      incrementByAmount: (state, action) => {
+        state.value += action.payload
+      },
+    },
+  })
+
+  export default taskSlice.reducer;
